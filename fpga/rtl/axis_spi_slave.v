9778c6c36f85a6aa68d385d9c3da0b38508e0ffd
Alex Forencich <alex@alexforencich.com>
2014-11-09 20:25:41 -0800

    Add AXI stream SPI slave

---
+++ fpga/rtl/axis_spi_slave.v
@@ -0,0 +1,242 @@
+/*
+
+Copyright (c) 2014 Alex Forencich
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+*/
+
+// Language: Verilog 2001
+
+`timescale 1ns / 1ps
+
+/*
+ * AXI4-Stream SPI Slave
+ */
+module axis_spi_slave #
+(
+    parameter DATA_WIDTH = 8
+)
+(
+    input  wire                   clk,
+    input  wire                   rst,
+
+    /*
+     * AXI input
+     */
+    input  wire [DATA_WIDTH-1:0]  input_axis_tdata,
+    input  wire                   input_axis_tvalid,
+    output wire                   input_axis_tready,
+    input  wire                   input_axis_tlast,
+
+    /*
+     * AXI output
+     */
+    output wire [DATA_WIDTH-1:0]  output_axis_tdata,
+    output wire                   output_axis_tvalid,
+    input  wire                   output_axis_tready,
+    output wire                   output_axis_tlast,
+
+    /*
+     * SPI interface
+     */
+    input  wire                   cs,
+    input  wire                   sck,
+    input  wire                   mosi,
+    output wire                   miso,
+
+    /*
+     * Status
+     */
+    output wire                   busy
+);
+
+// state register
+localparam [1:0]
+    STATE_IDLE = 2'd0,
+    STATE_TRANSFER = 2'd1,
+    STATE_WAIT = 2'd2;
+
+reg [1:0] state_reg = STATE_IDLE, state_next;
+
+reg [DATA_WIDTH:0] tx_data_reg = 0, tx_data_next;
+reg [DATA_WIDTH:0] rx_data_reg = 0, rx_data_next;
+reg [4:0] bit_cnt_reg = 0, bit_cnt_next;
+
+reg input_axis_tready_reg = 0, input_axis_tready_next;
+
+reg [DATA_WIDTH-1:0] output_axis_tdata_reg = 0, output_axis_tdata_next;
+reg output_axis_tvalid_reg = 0, output_axis_tvalid_next;
+reg output_axis_tlast_reg = 0, output_axis_tlast_next;
+
+reg miso_reg = 0, miso_next;
+
+reg busy_reg = 0;
+
+assign input_axis_tready = input_axis_tready_reg;
+
+assign output_axis_tdata = output_axis_tdata_reg;
+assign output_axis_tvalid = output_axis_tvalid_reg;
+assign output_axis_tlast = output_axis_tlast_reg;
+
+assign miso = miso_reg;
+
+assign busy = busy_reg;
+
+reg sck_last = 0;
+wire sck_posedge = sck & !sck_last;
+wire sck_negedge = !sck & sck_last;
+
+always @* begin
+    state_next = 2'bz;
+
+    tx_data_next = tx_data_reg;
+    rx_data_next = rx_data_reg;
+
+    bit_cnt_next = bit_cnt_reg;
+
+    input_axis_tready_next = 0;
+
+    output_axis_tdata_next = output_axis_tdata_reg;
+    output_axis_tvalid_next = output_axis_tvalid_reg & ~output_axis_tready;
+    output_axis_tlast_next = output_axis_tlast_reg;
+
+    miso_next = miso_reg;
+
+    case (state_reg)
+        STATE_IDLE: begin
+            if (cs) begin
+                // cs deasserted
+                miso_next = 0;
+                state_next = STATE_IDLE;
+            end else begin
+                // cs asserted - start of frame
+                if (input_axis_tvalid) begin
+                    // input data valid, read it in
+                    {miso_next, tx_data_next[DATA_WIDTH-1:1]} = input_axis_tdata;
+                    input_axis_tready_next = 1;
+                end else begin
+                    // input data not valid, send filler
+                    miso_next = 0;
+                    tx_data_next = 0;
+                end
+                rx_data_next = 0;
+                bit_cnt_next = DATA_WIDTH-1;
+                state_next = STATE_TRANSFER;
+            end
+        end
+        STATE_TRANSFER: begin
+            if (sck_posedge) begin
+                // rising sck edge, read mosi
+                rx_data_next = {rx_data_reg[DATA_WIDTH-2:0], mosi};
+                state_next = STATE_TRANSFER;
+            end else if (sck_negedge) begin
+                // falling sck edge, write miso
+                if (bit_cnt_reg > 0) begin
+                    // not last edge, pull from buffer
+                    {miso_next, tx_data_next[DATA_WIDTH-1:1]} = tx_data_reg;
+                    bit_cnt_next = bit_cnt_reg - 1;
+                    state_next = STATE_TRANSFER;
+                end else begin
+                    // last falling edge, look ahead
+                    if (input_axis_tvalid) begin
+                        // grab next bit from buffer if available
+                        {miso_next, tx_data_next[DATA_WIDTH-1:1]} = input_axis_tdata;
+                    end else begin
+                        // otherwise send filler
+                        miso_next = 0;
+                        tx_data_next = 0;
+                    end
+                    // wait for next event (don't know if end of frame yet)
+                    state_next = STATE_WAIT;
+                end
+            end else if (cs) begin
+                // premature end of frame
+                output_axis_tdata_next = 0;
+                output_axis_tvalid_next = 1;
+                output_axis_tlast_next = 1;
+                state_next = STATE_IDLE;
+            end else begin
+                state_next = STATE_TRANSFER;
+            end
+        end
+        STATE_WAIT: begin
+            if (cs) begin
+                // end of frame, transfer out
+                output_axis_tdata_next = rx_data_reg;
+                output_axis_tvalid_next = 1;
+                output_axis_tlast_next = 1;
+                state_next = STATE_IDLE;
+            end else if (sck_posedge) begin
+                // continuing frame, transfer out
+                output_axis_tdata_next = rx_data_reg;
+                output_axis_tvalid_next = 1;
+                output_axis_tlast_next = 0;
+                // read in new byte
+                if (input_axis_tvalid) begin
+                    {miso_next, tx_data_next[DATA_WIDTH-1:1]} = input_axis_tdata;
+                    input_axis_tready_next = 1;
+                end else begin
+                    miso_next = 0;
+                    tx_data_next = 0;
+                end
+                rx_data_next = mosi;
+                bit_cnt_next = DATA_WIDTH-1;
+                state_next = STATE_TRANSFER;
+            end else begin
+                state_next = STATE_WAIT;
+            end
+        end
+    endcase
+end
+
+always @(posedge clk or posedge rst) begin
+    if (rst) begin
+        state_reg <= STATE_IDLE;
+        tx_data_reg <= 0;
+        rx_data_reg <= 0;
+        bit_cnt_reg <= 0;
+        sck_last <= 0;
+        input_axis_tready_reg <= 0;
+        output_axis_tdata_reg <= 0;
+        output_axis_tvalid_reg <= 0;
+        output_axis_tlast_reg <= 0;
+        miso_reg <= 0;
+        busy_reg <= 0;
+    end else begin
+        state_reg <= state_next;
+
+        tx_data_reg <= tx_data_next;
+        rx_data_reg <= rx_data_next;
+
+        bit_cnt_reg <= bit_cnt_next;
+
+        sck_last <= sck;
+
+        input_axis_tready_reg <= input_axis_tready_next;
+
+        output_axis_tdata_reg <= output_axis_tdata_next;
+        output_axis_tvalid_reg <= output_axis_tvalid_next;
+        output_axis_tlast_reg <= output_axis_tlast_next;
+
+        miso_reg <= miso_next;
+    end
+end
+
+endmodule
---
+++ fpga/tb/test_axis_spi_slave.py
@@ -0,0 +1,306 @@
+#!/usr/bin/env python2
+"""
+
+Copyright (c) 2014 Alex Forencich
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+"""
+
+from myhdl import *
+import os
+from Queue import Queue
+
+import axis_ep
+import spi_ep
+
+module = 'axis_spi_slave'
+
+srcs = []
+
+srcs.append("../rtl/%s.v" % module)
+srcs.append("test_%s.v" % module)
+
+src = ' '.join(srcs)
+
+build_cmd = "iverilog -o test_%s.vvp %s" % (module, src)
+
+def dut_axis_spi_slave(clk,
+                 rst,
+                 current_test,
+
+                 input_axis_tdata,
+                 input_axis_tvalid,
+                 input_axis_tready,
+                 input_axis_tlast,
+
+                 output_axis_tdata,
+                 output_axis_tvalid,
+                 output_axis_tready,
+                 output_axis_tlast,
+
+                 cs,
+                 sck,
+                 mosi,
+                 miso,
+
+                 busy):
+
+    if os.system(build_cmd):
+        raise Exception("Error running build command")
+    return Cosimulation("vvp -m myhdl test_%s.vvp -lxt2" % module,
+                clk=clk,
+                rst=rst,
+                current_test=current_test,
+
+                input_axis_tdata=input_axis_tdata,
+                input_axis_tvalid=input_axis_tvalid,
+                input_axis_tready=input_axis_tready,
+                input_axis_tlast=input_axis_tlast,
+
+                output_axis_tdata=output_axis_tdata,
+                output_axis_tvalid=output_axis_tvalid,
+                output_axis_tready=output_axis_tready,
+                output_axis_tlast=output_axis_tlast,
+
+                cs=cs,
+                sck=sck,
+                mosi=mosi,
+                miso=miso,
+
+                busy=busy)
+
+def bench():
+
+    # Inputs
+    clk = Signal(bool(0))
+    rst = Signal(bool(0))
+    current_test = Signal(intbv(0)[8:])
+
+    input_axis_tdata = Signal(intbv(0)[8:])
+    input_axis_tvalid = Signal(bool(0))
+    input_axis_tlast = Signal(bool(0))
+    output_axis_tready = Signal(bool(0))
+    cs = Signal(bool(0))
+    sck = Signal(bool(0))
+    mosi = Signal(bool(0))
+
+    # Outputs
+    input_axis_tready = Signal(bool(0))
+    output_axis_tdata = Signal(intbv(0)[8:])
+    output_axis_tvalid = Signal(bool(0))
+    output_axis_tlast = Signal(bool(0))
+    miso = Signal(bool(0))
+
+    busy = Signal(bool(0))
+
+    # sources and sinks
+    source_queue = Queue()
+    source_pause = Signal(bool(0))
+    sink_queue = Queue()
+    sink_pause = Signal(bool(0))
+
+    source = axis_ep.AXIStreamSource(clk,
+                                     rst,
+                                     tdata=input_axis_tdata,
+                                     tvalid=input_axis_tvalid,
+                                     tready=input_axis_tready,
+                                     tlast=input_axis_tlast,
+                                     fifo=source_queue,
+                                     pause=source_pause,
+                                     name='source')
+
+    sink = axis_ep.AXIStreamSink(clk,
+                                 rst,
+                                 tdata=output_axis_tdata,
+                                 tvalid=output_axis_tvalid,
+                                 tready=output_axis_tready,
+                                 tlast=output_axis_tlast,
+                                 fifo=sink_queue,
+                                 pause=sink_pause,
+                                 name='sink')
+
+    # SPI master
+    master_tx_queue = Queue()
+    master_rx_queue = Queue()
+
+    master = spi_ep.SPIMaster(clk,
+                              rst,
+                              cs=cs,
+                              sck=sck,
+                              mosi=mosi,
+                              miso=miso,
+                              width=8,
+                              prescale=2,
+                              cpol=0,
+                              cpha=0,
+                              tx_fifo=master_tx_queue,
+                              rx_fifo=master_rx_queue,
+                              name='spi')
+
+    # DUT
+    dut = dut_axis_spi_slave(clk,
+                        rst,
+                        current_test,
+
+                        input_axis_tdata,
+                        input_axis_tvalid,
+                        input_axis_tready,
+                        input_axis_tlast,
+
+                        output_axis_tdata,
+                        output_axis_tvalid,
+                        output_axis_tready,
+                        output_axis_tlast,
+
+                        cs,
+                        sck,
+                        mosi,
+                        miso,
+
+                        busy)
+
+    @always(delay(4))
+    def clkgen():
+        clk.next = not clk
+
+    @instance
+    def check():
+        yield delay(100)
+        yield clk.posedge
+        rst.next = 1
+        yield clk.posedge
+        rst.next = 0
+        yield clk.posedge
+        yield delay(100)
+        yield clk.posedge
+
+        yield clk.posedge
+
+        while not sink_queue.empty():
+            sink_queue.get()
+        while not master_rx_queue.empty():
+            master_rx_queue.get()
+
+        yield clk.posedge
+        print("test 1: SPI to AXI walk")
+        current_test.next = 1
+
+        test_frame = bytearray('\x00\x01\x02\x04\x08\x10\x20\x40\x80')
+        master_tx_queue.put(test_frame)
+        yield clk.posedge
+
+        yield output_axis_tlast.posedge
+        yield clk.posedge
+        yield clk.posedge
+
+        rx_frame = None
+        if not sink_queue.empty():
+            rx_frame = bytearray(sink_queue.get())
+        assert rx_frame == test_frame
+
+        yield delay(100)
+
+        while not sink_queue.empty():
+            sink_queue.get()
+        while not master_rx_queue.empty():
+            master_rx_queue.get()
+
+        yield clk.posedge
+        print("test 2: SPI to AXI walk 2")
+        current_test.next = 2
+
+        test_frame = bytearray('\x00\x01\x03\x07\x0F\x1F\x3F\x7F\xFF')
+        master_tx_queue.put(test_frame)
+        yield clk.posedge
+
+        yield output_axis_tlast.posedge
+        yield clk.posedge
+        yield clk.posedge
+
+        rx_frame = None
+        if not sink_queue.empty():
+            rx_frame = bytearray(sink_queue.get())
+        assert rx_frame == test_frame
+
+        yield delay(100)
+
+        while not sink_queue.empty():
+            sink_queue.get()
+        while not master_rx_queue.empty():
+            master_rx_queue.get()
+
+        yield clk.posedge
+        print("test 3: AXI to SPI walk")
+        current_test.next = 3
+
+        test_frame = bytearray('\x00\x01\x02\x04\x08\x10\x20\x40\x80')
+        master_tx_queue.put(bytearray('\x00'*9))
+        source_queue.put(test_frame)
+        yield clk.posedge
+
+        yield output_axis_tlast.posedge
+        yield clk.posedge
+        yield clk.posedge
+
+        rx_frame = None
+        if not master_rx_queue.empty():
+            rx_frame = bytearray(master_rx_queue.get())
+        assert rx_frame == test_frame
+
+        yield delay(100)
+
+        while not sink_queue.empty():
+            sink_queue.get()
+        while not master_rx_queue.empty():
+            master_rx_queue.get()
+
+        yield clk.posedge
+        print("test 4: AXI to SPI walk 2")
+        current_test.next = 4
+
+        test_frame = bytearray('\x00\x01\x03\x07\x0F\x1F\x3F\x7F\xFF')
+        master_tx_queue.put(bytearray('\x00'*9))
+        source_queue.put(test_frame)
+        yield clk.posedge
+
+        yield output_axis_tlast.posedge
+        yield clk.posedge
+        yield clk.posedge
+
+        rx_frame = None
+        if not master_rx_queue.empty():
+            rx_frame = bytearray(master_rx_queue.get())
+        assert rx_frame == test_frame
+
+        yield delay(100)
+
+        raise StopSimulation
+
+    return dut, source, sink, master, clkgen, check
+
+def test_bench():
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    sim = Simulation(bench())
+    sim.run()
+
+if __name__ == '__main__':
+    print("Running test...")
+    test_bench()
+
