f2a364c1d32abc71b1b4fa8df9ab943398645a13
Alex Forencich <alex@alexforencich.com>
2014-11-23 21:20:36 -0800

    Use 10 MHz output of PLL for ref out, convert tabs to spaces

--- fpga/rtl/fpga.v
+++ fpga/rtl/fpga.v
@@ -31,98 +31,98 @@ THE SOFTWARE.
  */
 module fpga
 (
-	// clocks
-	input  wire  clk_10mhz_int,
-	input  wire  clk_10mhz_ext,
-	output wire  clk_10mhz_out,
+    // clocks
+    input  wire  clk_10mhz_int,
+    input  wire  clk_10mhz_ext,
+    output wire  clk_10mhz_out,
 
-	// SoC interface
-	input  wire  cntrl_cs,
-	input  wire  cntrl_sck,
-	input  wire  cntrl_mosi,
-	output wire  cntrl_miso,
+    // SoC interface
+    input  wire  cntrl_cs,
+    input  wire  cntrl_sck,
+    input  wire  cntrl_mosi,
+    output wire  cntrl_miso,
 
-	// Trigger
-	input  wire  ext_trig,
+    // Trigger
+    input  wire  ext_trig,
 
-	// Frequency counter
-	input  wire  ext_prescale,
+    // Frequency counter
+    input  wire  ext_prescale,
 
-	// Front end relay control
-	output wire  ferc_dat,
-	output wire  ferc_clk,
-	output wire  ferc_lat,
+    // Front end relay control
+    output wire  ferc_dat,
+    output wire  ferc_clk,
+    output wire  ferc_lat,
 
-	// Analog mux
-	output wire  [2:0] mux_s,
+    // Analog mux
+    output wire  [2:0] mux_s,
 
-	// ADC
-	output wire  adc_sclk,
-	input  wire  adc_sdo,
-	output wire  adc_sdi,
-	output wire  adc_cs,
-	input  wire  adc_eoc,
-	output wire  adc_convst,
+    // ADC
+    output wire  adc_sclk,
+    input  wire  adc_sdo,
+    output wire  adc_sdi,
+    output wire  adc_cs,
+    input  wire  adc_eoc,
+    output wire  adc_convst,
 
-	// digital output
-	output wire [15:0] dout,
+    // digital output
+    output wire [15:0] dout,
 
-	// Sync DAC
-	output wire [7:0] sync_dac,
+    // Sync DAC
+    output wire [7:0] sync_dac,
 
-	// Main DAC
-	output wire dac_clk_p,
-	output wire dac_clk_n,
-	output wire [15:0] dac_p1_d,
-	output wire [15:0] dac_p2_d,
-	input  wire dac_sdo,
-	output wire dac_sdio,
-	output wire dac_sclk,
-	output wire dac_csb,
-	output wire dac_reset,
+    // Main DAC
+    output wire dac_clk_p,
+    output wire dac_clk_n,
+    output wire [15:0] dac_p1_d,
+    output wire [15:0] dac_p2_d,
+    input  wire dac_sdo,
+    output wire dac_sdio,
+    output wire dac_sclk,
+    output wire dac_csb,
+    output wire dac_reset,
 
-	// ram 1 (U8)
-	output wire ram1_cke,
-	output wire ram1_ck_p,
-	output wire ram1_ck_n,
-	output wire ram1_cs_n,
-	output wire ram1_ras_n,
-	output wire ram1_cas_n,
-	output wire ram1_we_n,
-	output wire [12:0] ram1_a,
-	output wire [2:0]  ram1_ba,
-	inout  wire [15:0] ram1_dq,
-	inout  wire ram1_ldqs_p,
-	inout  wire ram1_ldqs_n,
-	output wire ram1_ldm,
-	inout  wire ram1_udqs_p,
-	inout  wire ram1_udqs_n,
-	output wire ram1_udm,
-	output wire ram1_odt,
-	inout  wire ram1_rzq,
-	inout  wire ram1_zio,
+    // ram 1 (U8)
+    output wire ram1_cke,
+    output wire ram1_ck_p,
+    output wire ram1_ck_n,
+    output wire ram1_cs_n,
+    output wire ram1_ras_n,
+    output wire ram1_cas_n,
+    output wire ram1_we_n,
+    output wire [12:0] ram1_a,
+    output wire [2:0]  ram1_ba,
+    inout  wire [15:0] ram1_dq,
+    inout  wire ram1_ldqs_p,
+    inout  wire ram1_ldqs_n,
+    output wire ram1_ldm,
+    inout  wire ram1_udqs_p,
+    inout  wire ram1_udqs_n,
+    output wire ram1_udm,
+    output wire ram1_odt,
+    inout  wire ram1_rzq,
+    inout  wire ram1_zio,
 
 
-	// ram 1 (U12)
-	output wire ram2_cke,
-	output wire ram2_ck_p,
-	output wire ram2_ck_n,
-	output wire ram2_cs_n,
-	output wire ram2_ras_n,
-	output wire ram2_cas_n,
-	output wire ram2_we_n,
-	output wire [12:0] ram2_a,
-	output wire [2:0]  ram2_ba,
-	inout  wire [15:0] ram2_dq,
-	inout  wire ram2_ldqs_p,
-	inout  wire ram2_ldqs_n,
-	output wire ram2_ldm,
-	inout  wire ram2_udqs_p,
-	inout  wire ram2_udqs_n,
-	output wire ram2_udm,
-	output wire ram2_odt,
-	inout  wire ram2_rzq,
-	inout  wire ram2_zio
+    // ram 1 (U12)
+    output wire ram2_cke,
+    output wire ram2_ck_p,
+    output wire ram2_ck_n,
+    output wire ram2_cs_n,
+    output wire ram2_ras_n,
+    output wire ram2_cas_n,
+    output wire ram2_we_n,
+    output wire [12:0] ram2_a,
+    output wire [2:0]  ram2_ba,
+    inout  wire [15:0] ram2_dq,
+    inout  wire ram2_ldqs_p,
+    inout  wire ram2_ldqs_n,
+    output wire ram2_ldm,
+    inout  wire ram2_udqs_p,
+    inout  wire ram2_udqs_n,
+    output wire ram2_udm,
+    output wire ram2_odt,
+    inout  wire ram2_rzq,
+    inout  wire ram2_zio
 );
 
 wire clk_250mhz_int;
@@ -131,59 +131,42 @@ wire rst_250mhz_int;
 wire clk_250mhz;
 wire rst_250mhz;
 
+wire clk_10mhz;
+wire rst_10mhz;
+
 wire ext_clock_selected;
 
 clock
 clock_inst
 (
-	.reset_in(0),
+    .reset_in(0),
 
-	.clk_10mhz_int(clk_10mhz_int),
-	.clk_10mhz_ext(clk_10mhz_ext),
+    .clk_10mhz_int(clk_10mhz_int),
+    .clk_10mhz_ext(clk_10mhz_ext),
 
-	.clk_250mhz_int(clk_250mhz_int),
-	.rst_250mhz_int(rst_250mhz_int),
+    .clk_250mhz_int(clk_250mhz_int),
+    .rst_250mhz_int(rst_250mhz_int),
 
-	.clk_250mhz(clk_250mhz),
-	.rst_250mhz(rst_250mhz),
+    .clk_250mhz(clk_250mhz),
+    .rst_250mhz(rst_250mhz),
 
-	.ext_clock_selected(ext_clock_selected)
+    .clk_10mhz(clk_10mhz),
+    .rst_10mhz(rst_10mhz),
+
+    .ext_clock_selected(ext_clock_selected)
 );
-
-
-// generate 10 MHz ref out
-// 25 cycle counter, falling edge interpolated
-reg [4:0] count_reg = 0;
-reg q0 = 0;
-reg q1 = 0;
-
-always @(posedge clk_250mhz or posedge rst_250mhz) begin
-    if (rst_250mhz) begin
-        count_reg <= 0;
-        q0 <= 0;
-        q1 <= 0;
-    end else begin
-        if (count_reg < 24) begin
-            count_reg <= count_reg + 1;
-        end else begin
-            count_reg <= 0;
-        end
-        q0 <= count_reg < 12;
-        q1 <= count_reg < 13;
-    end
-end
 
 ODDR2
 clk_10mhz_out_oddr2_inst
 (
-	.Q(clk_10mhz_out),
-	.C0(clk_250mhz),
-	.C1(~clk_250mhz),
-	.CE(1),
-	.D0(q0),
-	.D1(q1),
-	.R(0),
-	.S(0)
+    .Q(clk_10mhz_out),
+    .C0(clk_10mhz),
+    .C1(~clk_10mhz),
+    .CE(1),
+    .D0(0),
+    .D1(1),
+    .R(0),
+    .S(0)
 );
 
 reg [15:0] count = 0;
@@ -204,30 +187,30 @@ wire dac_clk;
 ODDR2
 dac_clk_oddr2_inst
 (
-	.Q(dac_clk),
-	.C0(clk_250mhz),
-	.C1(~clk_250mhz),
-	.CE(1),
-	.D0(0),
-	.D1(1),
-	.R(0),
-	.S(0)
+    .Q(dac_clk),
+    .C0(clk_250mhz),
+    .C1(~clk_250mhz),
+    .CE(1),
+    .D0(0),
+    .D1(1),
+    .R(0),
+    .S(0)
 );
 
 OBUFDS
 dac_clk_obufds_inst
 (
-	.I(dac_clk),
-	.O(dac_clk_p),
-	.OB(dac_clk_n)
+    .I(dac_clk),
+    .O(dac_clk_p),
+    .OB(dac_clk_n)
 );
 
 reg [15:0] dac_p1_d_reg = 0;
 reg [15:0] dac_p2_d_reg = 0;
 
 always @(posedge clk_250mhz) begin
-	dac_p1_d_reg <= count;
-	dac_p2_d_reg <= -count;
+    dac_p1_d_reg <= count;
+    dac_p2_d_reg <= -count;
 end
 
 (* IOB = "TRUE" *)
@@ -236,8 +219,8 @@ reg [15:0] dac_p1_d_oreg;
 reg [15:0] dac_p2_d_oreg;
 
 always @(posedge clk_250mhz) begin
-	dac_p1_d_oreg <= dac_p1_d_reg;
-	dac_p2_d_oreg <= dac_p2_d_reg;
+    dac_p1_d_oreg <= dac_p1_d_reg;
+    dac_p2_d_oreg <= dac_p2_d_reg;
 end
 
 assign dac_p1_d = dac_p1_d_oreg;
@@ -253,7 +236,7 @@ assign dout = count;
 
 
 always @(posedge clk_250mhz) begin
-	count <= count + 1;
+    count <= count + 1;
 end
 
 /////////////////////////////////////////////////
@@ -320,13 +303,13 @@ wire ram2_p0_rd_error;
 ddr2_clock
 ddr2_clock_inst
 (
-	.clk_250mhz(clk_250mhz_int),
-	.rst_250mhz(rst_250mhz_int),
+    .clk_250mhz(clk_250mhz_int),
+    .rst_250mhz(rst_250mhz_int),
 
-	.mcb_clk_0(mcb_clk_0),
-	.mcb_clk_180(mcb_clk_180),
-	.mcb_drp_clk(mcb_drp_clk),
-	.mcb_clk_locked(mcb_clk_locked)
+    .mcb_clk_0(mcb_clk_0),
+    .mcb_clk_180(mcb_clk_180),
+    .mcb_drp_clk(mcb_drp_clk),
+    .mcb_clk_locked(mcb_clk_locked)
 );
 
 ddr2
